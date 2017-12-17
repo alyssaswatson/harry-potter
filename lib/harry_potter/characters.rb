9776e314a07274d5fa620a99437a33186f04e867
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-12-17 18:14:52 +0000

    scrape data being added to instace of object works

--- lib/harry_potter/characters.rb
+++ lib/harry_potter/characters.rb
@@ -1,30 +1,23 @@
 class HarryPotter::Characters
 
-  attr_accessor :name, :gender, :house, :species, :character, :date_birth, :year_birth, :ancestry, :character
+  attr_reader :name, :gender, :house, :species, :character, :date_birth, :year_birth, :ancestry, :character
 
   @@all = []
 
-  # def character
-  #   harry = {"name":"Harry Potter","species":"human","gender":"male","house":"Gryffindor","dateOfBirth":"31-07-1980","yearOfBirth":1980,"ancestry":"half-blood","eyeColour":"green","hairColour":"black","wand":{"wood":"holly","core":"phoenix feather","length":11},"patronus":"stag","hogwartsStudent":true,"hogwartsStaff":false,"actor":"Daniel Radcliffe","alive":true,"image":"http://hp-api.herokuapp.com/images/harry.jpg"}
-  #   #HarryPotter::API.character_list.each do |char|
-  #   @character.name = harry[:name]
-  #   @@all << self
-  # end
-
   def self.new_from_api(character_info)
-    #@character.new = self
-    # harry = {"name":"Harry Potter","species":"human","gender":"male","house":"Gryffindor","dateOfBirth":"31-07-1980","yearOfBirth":1980,"ancestry":"half-blood","eyeColour":"green","hairColour":"black","wand":{"wood":"holly","core":"phoenix feather","length":11},"patronus":"stag","hogwartsStudent":true,"hogwartsStaff":false,"actor":"Daniel Radcliffe","alive":true,"image":"http://hp-api.herokuapp.com/images/harry.jpg"}
     # harry.keys.each do |key|
     #   m = "#{key}="
     #   @character.send( m, hash[key] ) if @character.respond_to?( m )
     # end
     #   @@all << self
-    binding.pry
+
     self.new(
-      character_info["name"],
-      character_info["species"],
-      character_info["gender"]
+      name = character_info["name"],
+      species = character_info["species"],
+      gender = character_info["gender"]
     )
+    @@all << self
+    binding.pry
   end
 
   def initialize(name = nil, species = nil, gender = nil)
@@ -40,9 +33,8 @@ class HarryPotter::Characters
   end
 
   def self.all_students
-    #puts "list of students"
+    puts "list of students"
     @@all
-    #binding.pry
   end
 
   def self.all_staff
